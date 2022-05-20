11ee1ee1910dd7a6512e851a6cc2efa5459301ff
IAMTHEMACHINE33 <iamthemachine33@gmail.com>
2022-05-20 11:32:05 +0545

    Dashboard listtiles,stacks

--- lib/screens/dashboard_scren.dart
+++ lib/screens/dashboard_scren.dart
@@ -11,11 +11,94 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
      return Scaffold(
-      body: Container(
-        child: Container(
-          color: Colors.amber,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 10,
+              child: ListTile(
+                title: const Text('Username'),
+                leading: CircleAvatar(backgroundColor: Colors.black54),
+                trailing: Icon(Icons.add),
+              ),
+            ),
+            Expanded(
+              flex: 20,
+              child:Stack(
+                children: [
+                  Container(
+                    color: Colors.green[100],
+                  ),
+                  Positioned(
+                    top: 30,
+                    left: 80,
+                    
+                    child: Container(
+                      child: const Text('Total'),
+                      
+                    ),
+                  ),
+                  Positioned(
+                    top: 60,
+                    left: 84,
+                    
+                    child: Container(
+                      child: Text('\$\$\$'),
+                    ),
+                  ),
+                  Positioned(
+                    top: 45,
+                    right: 50,
+                    child: Container(
+                      color: Colors.green[300],
+                      child: Container(
+                        margin: EdgeInsets.all(10),
+                        child: Text('52.4%'),
+                      ),
+                    ),
+                  )
+
+                ],
+              )
+            ),
+            Expanded(
+              flex:30,
+              child: Container(),
+            ),
+            Expanded(
+              flex: 3,
+              child: SizedBox(
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: const Text('Top Performer')
+                  ),
+              )
+                ),
+            Expanded(
+              flex:35,
+              child: ListView.builder(
+                itemCount: 4,
+                itemBuilder: (context,int index){
+                  return Container(
+                    child: ListTile(
+                      title: const Text('Ticker'),
+                      subtitle: const Text('Name'),
+                      trailing: const Text('55%')
+                    ),
+                  );
+                }),
+            ),
+            Expanded(
+              flex: 10,
+              child: Row(
+                children: [
+                  Container()
+                ],
+              ),
+            ),
+          ],
         ),
-      ),
+      )
     );
   }
 }
